2e45af626a3138e2cd16c25bc109c7469da6bebc
marc <marc@refamco.ca>
2016-03-09 09:43:49 -0500

    update version

--- django_markdown/static/django_markdown/jquery.init.js
+++ django_markdown/static/django_markdown/jquery.init.js
@@ -1,6 +1,7 @@
 // Starting from Django 1.6+, jQuery is only available as 'django' object attribute.
 // Unfortunately, it's not compatible with markitup.js library because it needs global jQuery variable to be initialized.
-jQuery = jQuery || django.jQuery;
+// don't need, just ensure we always load jquery!
+//jQuery = jQuery || django.jQuery;
 
 (function($) {
     // from http://docs.djangoproject.com/en/1.4/ref/contrib/csrf/#ajax
